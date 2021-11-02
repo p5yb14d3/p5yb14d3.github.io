3bc50c6894984b90c40333c9a7c2452d0ff12595
p5yb14d3 <p5yb14d3@gmail.com>
2021-11-03 05:29:47 +0800

    Add files via upload

---
+++ CodingSchool/lesson1.js
@@ -0,0 +1,9 @@
+<div id="lesson1" class="lesson">
+	<h1>Lesson 1: Axis on the Canvas</h1>
+	
+	<p>Hover the mouse over the screen to get its x and y axis position.</p>
+	<p>The blue horizontal ruler is the x axis, and the red vertical ruler is the y axis.</p>
+	<p>The blue ruler's width is 600 pixels wide, and the red ruler's height is 400 pixels tall.</p>
+
+	<h3></h3>
+</div>
